772365f8f049cfb3965035c07e43a253d387f77e
Kamlesh Singh <kamleshsingh0198@gmail.com>
2025-01-22 05:28:44 +0530

    add routing

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,29 +1,35 @@
-import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' : 'light';
+  const paletteType = darkMode ? "dark" : "light";
 
   const theme = createTheme({
-    palette:{
-      mode:paletteType,
-    }
-  })
-  function handleThemeChange(){
+    palette: {
+      mode: paletteType,
+    },
+  });
+  function handleThemeChange() {
     setDarkMode(!darkMode);
   }
   return (
     <ThemeProvider theme={theme}>
-    <CssBaseline/>
-    <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
-    <Container sx={{ paddingTop: "64px" }}>
-      <Catalog/>
-    </Container>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container sx={{ paddingTop: "64px" }}>
+        <Outlet />
+      </Container>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,5 +1,36 @@
-import { Brightness4, Brightness7 } from "@mui/icons-material";
-import { AppBar, IconButton, Switch, Toolbar, Typography } from "@mui/material";
+import { Brightness4, Brightness7, ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const navLinks = [
+  { title: "Home", path: "/" },
+  { title: "Store", path: "/store" },
+  { title: "Contact", path: "/contact" },
+];
+const accountLinks = [
+  { title: "Login", path: "/login" },
+  { title: "Register", path: "/register" },
+];
+const navStyles = {
+  color: "inherit",
+  typography: "h6",
+  textDecoration: "none",
+  "&:hover": {
+    color: "secondary.main",
+  },
+  "&:active": {
+    color: "text.secondary",
+  },
+};
 
 interface Props {
   darkMode: boolean;
@@ -11,12 +42,40 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
       position="fixed"
       sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
     >
-      <Toolbar>
-        <Typography variant="h6">Sports Center</Typography>
-
-        <IconButton onClick={handleThemeChange} color="inherit">
-          {darkMode ? <Brightness7 /> : <Brightness4 />}
-        </IconButton>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          <Typography variant="h6">Sports Center</Typography>
+          <IconButton onClick={handleThemeChange} color="inherit">
+            {darkMode ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+        </Box>
+        <List sx={{ display: "flex" }}>
+          {navLinks.map(({ title, path }) => (
+            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+              {title}
+            </ListItem>
+          ))}
+        </List>
+        <Box display="flex" alignItems="center">
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+            <Badge badgeContent="4" color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {accountLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
