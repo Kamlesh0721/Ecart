911d8f1eca1aed9a130af6354e87d659045a3168
Kamlesh Singh <kamleshsingh0198@gmail.com>
2025-02-20 19:38:59 +0530

    add pagination and search

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import basketService from "./basketService";
 import { Dispatch } from "redux";
 import { Product } from "../models/product";
-import { Basket as BasketType } from "../models/basket";
+import { Basket } from "../models/basket";
 
 axios.defaults.baseURL ='http://localhost:8081/api/';
 
@@ -39,8 +39,21 @@ const requests = {
 }
 
 const Store = {
-    list:()=> requests.get('products'),
-    details:(id: number) => requests.get(`products/${id}`)
+    apiUrl: 'http://localhost:8081/api/products',
+    list:(page: number, size: number, brandId?: number, typeId?: number, url?: string)=> {
+      let requestUrl = url || `products?page=${page-1}&size=${size}`;
+      if(brandId!==undefined){
+        requestUrl += `&brandId=${brandId}`;
+      }
+      if(typeId!==undefined){
+        requestUrl += `&typeId=${typeId}`;
+      }
+      return requests.get(requestUrl);
+    },
+    details:(id: number) => requests.get(`products/${id}`),
+    types: () => requests.get('products/types').then(types => [{ id: 0, name: 'All' }, ...types]),
+    brands: () => requests.get('products/brands').then(brands => [{ id: 0, name: 'All' }, ...brands]),
+    search: (keyword: string) => requests.get(`products?keyword=${keyword}`)
 };
 
 const Basket = {
@@ -86,7 +99,7 @@ const Basket = {
           throw error;
         }
       },
-      setBasket: async (basket: BasketType, dispatch: Dispatch) => {
+      setBasket: async (basket: Basket, dispatch: Dispatch) => {
         try {
           await basketService.setBasket(basket, dispatch);
         } catch (error) {
